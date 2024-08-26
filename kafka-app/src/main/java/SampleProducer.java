31bfe4e7e8bc26049c5ea0ee5dd62c5694465b2b
Komal Vighe <44812233+Komalvighe@users.noreply.github.com>
2024-08-26 21:47:23 +0530

    Update SampleProducer.java

--- kafka-app/src/main/java/SampleProducer.java
+++ kafka-app/src/main/java/SampleProducer.java
@@ -8,8 +8,8 @@ public class SampleProducer {
   {
 	
 	Properties properties= new Properties();
+	
 	properties.put("bootstrap.servers","localhost:9092");
-	//properties.put("bootstrap.servers","localhost:9092");
 	properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
 	properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
 	properties.put("security.protocol", "SSL");
