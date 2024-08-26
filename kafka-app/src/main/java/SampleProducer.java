19ea1e7c5ea6fc0a69b2bdc7fdb14aa3ef2eeb71
Komal Vighe <44812233+Komalvighe@users.noreply.github.com>
2024-08-26 21:46:55 +0530

    Update SampleProducer.java

--- kafka-app/src/main/java/SampleProducer.java
+++ kafka-app/src/main/java/SampleProducer.java
@@ -8,7 +8,7 @@ public class SampleProducer {
   {
 	
 	Properties properties= new Properties();
-	properties.put("bootstrap.servers","my-cluster-kafka-listerner1-bootstrap-amq-streams-kafka.apps.komaltestcluster.lab.psi.pnq2.redhat.com:443");
+	properties.put("bootstrap.servers","localhost:9092");
 	//properties.put("bootstrap.servers","localhost:9092");
 	properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
 	properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
